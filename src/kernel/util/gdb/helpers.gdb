2392c51863b7a1676d92702a468aca9eb9f93f19
Tray Torrance <torrancew@gmail.com>
2016-08-12 19:36:20 -0700

    Tidy up helpers; add boundary vars to linker scripts

--- src/kernel/util/gdb/helpers.gdb
+++ src/kernel/util/gdb/helpers.gdb
@@ -1,5 +1,5 @@
 # vim: set ft=gdb:
 
-source util/gdb/helpers/multiboot.scm
+source util/gdb/helpers/printers.scm
 
 target remote localhost:1234
--- src/kernel/util/gdb/helpers/printers.scm
+++ src/kernel/util/gdb/helpers/printers.scm
@@ -4,13 +4,13 @@
   (gdb iterator)
   (gdb printing))
 
-(define (make-multiboot-struct-tostring value)
+(define (make-struct-tostring value)
   (lambda (printer)
     (format #f "~a<0x~8,'0x>"
             (type-name (value-type value))
             (value->integer (value-address value)))))
 
-(define (make-multiboot-struct-iterator value)
+(define (make-struct-iterator value)
   (lambda (printer)
     (make-list-iterator
       (map
@@ -18,21 +18,21 @@
           (cons (field-name  field) (value-field value (field-name field))))
         (type-fields (value-type value))))))
 
-(define (make-multiboot-struct-printer value)
+(define (make-struct-printer value)
   "Print multiboot structs"
   (make-pretty-printer-worker
     "map"
-    (make-multiboot-struct-tostring value)
-    (make-multiboot-struct-iterator value)))
+    (make-struct-tostring value)
+    (make-struct-iterator value)))
 
-(define (multiboot-struct-lookup-fn pp val)
+(define (struct-lookup-fn pp val)
   (let ((type (value-type val)))
     (and (or (= TYPE_CODE_STRUCT (type-code type))
              (and (= TYPE_CODE_TYPEDEF (type-code type))
                   (= TYPE_CODE_STRUCT  (type-code (type-target type)))))
          (string-prefix? "multiboot_" (type-name type))
-         (make-multiboot-struct-printer val))))
+         (make-struct-printer val))))
 
 (append-pretty-printer! #f
-                        (make-pretty-printer "multiboot-struct-printer"
-                                             multiboot-struct-lookup-fn))
+                        (make-pretty-printer "struct-printer"
+                                             struct-lookup-fn))
